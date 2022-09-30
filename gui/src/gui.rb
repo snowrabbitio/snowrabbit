83de6e1aa97e9896c9228e36c09c7c6235dfd689
Jason Camp <jason.camp1@accesscfa.com>
2022-09-30 09:55:26 -0400

    Splitting initial master (which listens to probe requests) from gui

--- gui/src/gui.rb
+++ gui/src/gui.rb
@@ -117,114 +117,7 @@ end
 
 # URL Actions
 get '/' do
-  'Snowrabbit'
-end
-
-post '/pang' do
-  # This will validate that a probe can reach the master and validates its secret
-  # See if this probe is registered
-  probes_registered= DB_CONNECTION[:probes].where(site: params[:site], active: 0..1)
-  probes_unregistered = DB_CONNECTION[:probes].where(site: params[:site], active: 2)
-
-  # See if this site is registered or unregistered
-  pang_authed = false
-  probes_registered.each do |probe|
-    if probe[:secret] == params[:secret]
-      pang_authed = true
-    end
-  end
-
-  # See if this probe is unregistered, if not mark it as unregistered
-  if !pang_authed && (probes_registered.count == 0) && (probes_unregistered.count == 0)
-    DB_CONNECTION[:probes].insert(site: params[:site], ip: request.ip, active: 2)
-  end
-
-  if pang_authed
-    'OK'
-  else
-    status 401
-     'Unauthorized'
-  end
-end
-
-get '/send_metric' do
-  'Error, must POST to this endpoint'
-end
-
-post '/send_metric' do
-  # Accepts a metric from a probe
-  LOGGER.debug("Starting send_metric")
-  metric = OpenStruct.new
-
-  metric.name = params[:name]
-  metric.source_site = params[:source_site]
-  metric.dest_site = params[:site]
-  metric.ip = params[:ip]
-  metric.timestamp = params[:time]
-  metric.secret = params[:secret]
-
-  if metric.name == "ping"
-    metric.transmitted = params[:transmitted]
-    metric.received = params[:received]
-    metric.packet_loss = params[:packet_loss]
-    metric.min = params[:min]
-    metric.avg = params[:avg]
-    metric.max = params[:max]
-  elsif metric.name = "traceroute"
-    metric.traceroute = params[:traceroute]
-  end
-
-  # Let's make sure we have the correct secret
-  probe_secret = DB_CONNECTION[:probes].where(site: metric.source_site, active: 1).first
-  if !probe_secret
-    LOGGER.debug("No secret found for site #{metric.source_site}")
-    status 401
-    'Forbidden'   
-  elsif (metric.secret != probe_secret[:secret])
-    LOGGER.debug("secret failed, ignoring...")
-    status 401
-    'Forbidden'
-  elsif metric.secret == probe_secret[:secret]
-    LOGGER.debug("secret succeeded, continuing")
-    LOGGER.debug("VALUE: #{metric}")
-
-    if metric.name == "ping"
-      LOGGER.debug("Saving ping metric")
-      table = DB_CONNECTION[:ping_metrics]
-      table.insert(timestamp: metric.timestamp,
-                   source_site: metric.source_site,
-                   dest_site: metric.dest_site,
-                   dest_ip: metric.ip,
-                   transmitted: metric.transmitted,
-                   received: metric.received,
-                   packet_loss: metric.packet_loss,
-                   min: metric.min,
-                   avg: metric.avg,
-                   max: metric.max)
-
-      # Mark that we got a metric from this probe
-      DB_CONNECTION[:probes].where(site: metric.source_site).update(last_seen: Time.now().to_i)
-
-      'OK'
-    elsif metric.name == "traceroute"
-      LOGGER.debug("Saving traceroute metric")
-      table = DB_CONNECTION[:traceroute_metrics]
-      table.insert(timestamp: metric.timestamp,
-                   source_site: metric.source_site,
-                   dest_site: metric.dest_site,
-                   dest_ip: metric.ip,
-                   traceroute: metric.traceroute)
-
-      # Mark that we got a metric from this probe
-      DB_CONNECTION[:probes].where(site: metric.source_site).update(last_seen: Time.now().to_i)
-
-      'OK'
-    else
-      status 401
-      'Forbidden'
-    end
-
-  end
+  redirect '/matrix'
 end
 
 
@@ -279,6 +172,37 @@ post '/register_probe' do
   DB_CONNECTION[:probes].where(site: site).update(secret: secret, active: 1)
 
   'Probe registered'
+end
+
+
+get '/matrix' do
+  # Get all of the latest ping times and display
+  @begin_time = Time.now
+  @probes_list = DB_CONNECTION[:probes].where(active: 1).order(Sequel.desc(:location), Sequel.asc(:site))
+  if @probes_list.count > 0
+    @probe_last_seen = @probes_list.order(Sequel.desc(:last_seen)).first[:last_seen]
+  end 
+
+  # Get the number of probes * 5 and double it just to be safe, i don't think we'll need more than that
+  @ping_table = DB_CONNECTION[:ping_metrics].limit(@probes_list.count * 5 * 2).order(Sequel.desc(:timestamp))
+
+  erb :matrix
+end
+
+
+get '/site_details' do
+  @source_site = params[:source_site]
+  @dest_site = params[:dest_site]
+  @ping_metrics = DB_CONNECTION[:ping_metrics].where(source_site: @source_site, dest_site: @dest_site).limit(5).order(Sequel.desc(:timestamp))
+  traceroute_metrics = DB_CONNECTION[:traceroute_metrics].where(source_site: @source_site, dest_site: @dest_site).limit(1).order(Sequel.desc(:timestamp))
+
+  if traceroute_metrics.count > 0
+    @traceroute_out = traceroute_metrics.first
+  else
+    @traceroute_out = "No traceroute found."
+  end
+
+  erb :site_details
 end
 
 get '/healthcheck' do
